cb7f60ff05c81d0264e2493462d014f78417cd45
emma-gardecki-FDA <emma.gardecki@fda.hhs.gov>
2024-08-23 13:04:27 -0400

    Script to make simplified data frame

---
+++ inst/extra/pilotHTT-RST/pilotHTT_cleaning.R
@@ -0,0 +1,47 @@
+# Edit pilotHTT data for RST
+# Remove extra columns and rename colnames for clarity
+
+
+# Load data ####
+load("C:/000-github/HTT/inst/extra/pilotHTT-RST/pilotHTT-rst-20220506.rda")
+
+pilotHTT_RST <- pilotHTT
+
+# Remove extra columns ####
+# Remove: score, createDate, task, inputFileName,
+  # percentStroma (40% missing), viewerHeight (21% missing),
+  # viewerWidth (21% missing), viewerMag (31% missing)
+
+pilotHTT_RST <- subset(pilotHTT_RST, select = -score)
+pilotHTT_RST <- subset(pilotHTT_RST, select = -createDate)
+pilotHTT_RST <- subset(pilotHTT_RST, select = -task)
+pilotHTT_RST <- subset(pilotHTT_RST, select = -inputFileName)
+pilotHTT_RST <- subset(pilotHTT_RST, select = -percentStroma)
+pilotHTT_RST <- subset(pilotHTT_RST, select = -viewerHeight)
+pilotHTT_RST <- subset(pilotHTT_RST, select = -viewerWidth)
+pilotHTT_RST <- subset(pilotHTT_RST, select = -viewerMag)
+
+# Rename colnames ####
+# VTA -> evaluable
+names(pilotHTT_RST)[names(pilotHTT_RST) == "VTA"] <- "evaluable"
+
+# Reorder columns ####
+colOrder <- c("batch", "WSI", "caseID", "readerID", "modalityID",
+              "labelROI", "evaluable", "densityTILs",
+              "experience", "experienceResident")
+
+pilotHTT_RST <- pilotHTT_RST[, colOrder]
+
+
+# Save Data ####
+# Save data (.rda)
+usethis::use_data(pilotHTT_RST, overwrite = TRUE)
+
+# Save data (.csv)
+write.csv(
+  pilotHTT_RST,
+  "inst/extra/pilotHTT-RST/pilotHTT_RST.csv",
+  row.names = FALSE, quote = FALSE
+)
+
+
